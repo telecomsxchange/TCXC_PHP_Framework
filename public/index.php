217e480e4ecd7d689fe37f1103c6b78b520f8c5d
ajamous <ameedjamous@Ameeds-iMac.local>
2017-11-25 02:18:25 +0200

    Inital Framework Commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+  
+  /*TCXC Simple MVC Framework - 
+  Written by Ameed Jamous (Twitter: @ameedjamous)
+  */
+
+
+  require_once '../app/bootstrap.php';
+
+  // Init Core Library
+  $init = new Core;
